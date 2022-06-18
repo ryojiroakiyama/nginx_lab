c87e9b3973410008e15771f53a3d365ed71aaf8c
rakiyama <ryojiro25@gmail.com>
2022-06-18 18:26:57 +0900

    delete検証

---
+++ test.sh
@@ -0,0 +1,21 @@
+set -x
+export confpath="./nginx.conf"
+docker-compose up -d
+curl localhost:8080 -v
+docker-compose down
+
+export confpath="./delete.conf"
+mkdir deletedir
+echo "##\nin delete.html\n##" > deletedir/delete.html
+echo "##\nin index.html\n##" > deletedir/index.html
+docker-compose up -d
+curl localhost:8080/delete.html -v
+curl -X DELETE localhost:8080/delete.html -v
+curl localhost:8080/delete.html -v
+echo "##########"
+curl localhost:8080 -v
+curl -X DELETE localhost:8080 -v
+curl localhost:8080 -v
+docker-compose down
+
+# command: zsh test.sh &> log
